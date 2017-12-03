c1b1cdf8758924c410b71acde1c5752732175472
Elodie Lecostey <lecostey.e@gmail.com>
2017-12-04 00:23:00 +0100

    - ajax call only running when color of text is white

--- src/App.js
+++ src/App.js
@@ -12,46 +12,46 @@ class App extends Component {
             content: 'Random color App'
         };
 
-        this.changeColor = this.changeColor.bind(this);
+        this.toggleColor = this.toggleColor.bind(this);
         this.updateContent = this.updateContent.bind(this);
     }
 
-    changeColor() {
-        const that = this;
-        $.ajax({
-            method: 'GET',
-            url: "http://www.colr.org/json/color/random",
-            dataType: 'json',
-            success: function (response) {
-                that.setColor(response.colors[0].hex);
-            },
-            error: function () {
-                console.log('an error occured, couldnt get random color.');
-            }
-        });
-    }
-
-    setColor(randomColor) {
-        const color = this.state.color === 'white' ? `#${randomColor}` : 'white';
-        this.setState({color: color });
+    toggleColor() {
+        if (this.state.color === 'white') {
+            const that = this;
+            $.ajax({
+                method: 'GET',
+                url: "http://www.colr.org/json/color/random",
+                dataType: 'json',
+                success: function (response) {
+                    const randomColor = `#${response.colors[0].hex}`;
+                    that.setState({ color: randomColor });
+                },
+                error: function () {
+                    console.log('an error occured, couldnt get random color.');
+                }
+            });
+        } else {
+            this.setState({ color: 'white' });
+        }
     }
 
     updateContent(e) {
         const updatedContent = e.target.value;
-        this.setState({content: updatedContent});
+        this.setState({ content: updatedContent });
     }
 
     render() {
         return (
             <div className="App">
                 <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h2 id="component" style={{color:this.state.color}}>{this.state.content}</h2>
+                    <img src={ logo } className="App-logo" alt="logo"/>
+                    <h2 id="component" style={{ color:this.state.color }}>{ this.state.content }</h2>
                 </div>
                 <form className='form'>
-                    Modify text: <input id='newText' type="text" onChange={this.updateContent}/>
+                    Modify text: <input id='newText' type="text" onChange={ this.updateContent }/>
                 </form>
-                <button onClick={this.changeColor}>change color</button>
+                <button onClick={ this.toggleColor }>Toggle color</button>
             </div>
         );
     }
